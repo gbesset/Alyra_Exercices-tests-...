171f59d4dbe4ee09f633d74691cd9892343759cb
gbesset <guillaume.besset.pro@gmail.com>
2023-02-20 14:21:57 +0100

    FEAT: test unitaire javascript

---
+++ 3-tests unitaires/test_unitaire_javascript/test/TestSimpleStorage.js
@@ -0,0 +1,17 @@
+const SimpleStorage = artifacts.require("SimpleStorage");
+
+//Passe les compte car on en a besoin pour effectuer des transactions
+contract("SimpleStorage", (accounts) => {
+  //test unitaire
+  it("...should store the value 89.", async () => {
+    const simpleStorageInstance = await SimpleStorage.deployed();
+
+    // Set value of 89
+    await simpleStorageInstance.set(89, { from: accounts[0] });
+
+    // Get stored value
+    const storedData = await simpleStorageInstance.get.call();
+
+    assert.equal(storedData, 89, "The value 89 was not stored.");
+  });
+});
