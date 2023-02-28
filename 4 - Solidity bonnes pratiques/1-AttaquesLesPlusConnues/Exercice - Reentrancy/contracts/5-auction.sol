58161fec71237b7031254f8b6f6c1b53253aabed
gbesset <guillaume.besset.pro@gmail.com>
2023-02-28 16:30:21 +0100

    FEAT: Attaque DoS inexpected (Enchère)

---
+++ 4 - Solidity bonnes pratiques/1-AttaquesLesPlusConnues/Exercice - Reentrancy/contracts/5-auction.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.8.9;
+
+contract auction {
+    address highestBidder;
+    uint highestBid;
+    mapping(address => uint) refunds;
+
+    function bid() payable external {
+        require(msg.value >= highestBid);
+
+        if (highestBidder != address(0)) {
+            refunds[highestBidder] += highestBid; // record the refund that this user can claim
+        }
+
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+    }
+
+    function withdrawRefund() external {
+        uint refund = refunds[msg.sender];
+        refunds[msg.sender] = 0;            // /!\ reentrency !!
+        (bool success, ) = msg.sender.call{value:refund}("");
+        require(success);
+    }
+}
